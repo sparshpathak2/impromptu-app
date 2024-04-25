be944f3d0eede181b8e4a497c43dbe7346db70cc
Sparsh <sparsh@Sparshs-MacBook-Air.local>
2024-04-25 21:24:56 +0530

    metadata added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,17 @@
 // Import styles of packages that you've installed.
 // All packages except `@mantine/hooks` require styles imports
 import '@mantine/core/styles.css';
+import logo from '../public/impromptu-icon-logo.png';
 
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import { HeaderMenu } from '@/components/HeaderMenu/HeaderMenu';
 import Provider from '@/components/Providers';
+import Head from 'next/head';
 
 export const metadata = {
-  title: 'My Mantine app',
-  description: 'I have followed setup instructions carefully',
+  // icon: `${logo}`,
+  title: 'ImPromptu - Your AI Prompts Manager',
+  description: 'The only tool you need to manage your AI Prompts',
 };
 
 export default function RootLayout({
@@ -21,6 +24,12 @@ export default function RootLayout({
       <head>
         <ColorSchemeScript />
       </head>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="favicon" href={logo.src} />
+        {/* <ColorSchemeScript /> */}
+      </Head>
       <body>
         <Provider session={undefined}>
           <MantineProvider>
